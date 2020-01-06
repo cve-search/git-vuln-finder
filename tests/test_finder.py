81786106d027e34136de2a0ffef15be6d4b0b970
Cédric Bonhomme <cedric@cedricbonhomme.org>
2020-01-06 16:43:43 +0100

    checkout a repo

--- tests/test_finder.py
+++ tests/test_finder.py
@@ -4,6 +4,6 @@ from git_vuln_finder import find
 
 
 def test_find_vuln():
-    all_potential_vulnerabilities, all_cve_found, found = find("/home/cedric/git/curl")
+    all_potential_vulnerabilities, all_cve_found, found = find("test_repos/curl")
 
     assert len(list(all_cve_found)) == 63
