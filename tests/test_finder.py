865ee48d21f2ac7b1adaa04a95a123c1412c09d4
Cédric Bonhomme <cedric@cedricbonhomme.org>
2020-01-06 22:56:18 +0100

    forgot the fixture

--- tests/test_finder.py
+++ tests/test_finder.py
@@ -3,7 +3,7 @@
 from git_vuln_finder import find
 
 
-def test_find_vuln():
+def test_find_vuln(clone_curl):
     all_potential_vulnerabilities, all_cve_found, found = find("./test_repos/curl/")
 
     #assert len(list(all_cve_found)) == 64
