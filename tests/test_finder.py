ceb2c63458f24997dfb897b278014110b5ead9ed
Cédric Bonhomme <cedric@cedricbonhomme.org>
2020-01-06 16:29:45 +0100

    added basic test

---
+++ tests/test_finder.py
@@ -0,0 +1,9 @@
+
+
+from git_vuln_finder import find
+
+
+def test_find_vuln():
+    all_potential_vulnerabilities, all_cve_found, found = find("/home/cedric/git/curl")
+
+    assert len(list(all_cve_found)) == 63
